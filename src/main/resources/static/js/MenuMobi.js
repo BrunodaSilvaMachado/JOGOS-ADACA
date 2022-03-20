9f0fdb56f9d4ae2eaa10229d9b5d4196288335cc
Bruno da silva <brunosilvamachado@id.uff.br>
2022-03-20 11:11:50 -0300

    O menu dos jogos foi atualizado. Agora usa bootstrap.

--- src/main/resources/static/js/MenuMobi.js
+++ src/main/resources/static/js/MenuMobi.js
@@ -37,18 +37,35 @@ function mudaJogo(parametro){
     window.location.href = url;                     //muda a pagina toda
 }
 
-function configButton() {
-	var NumJogos = 65;
-	//daqui pra baixo são os jogos:
-	//array que configura os botões visiveis ou invisíveis.
-	var jogoVisib = new Array();
-	jogoVisib.length = 0;
-	var botao = null;
+function criarBotao(callback,id){
+    return "<div class=\"col\"><button class=\"btn btn-sm btn-default botao\" id=\"botao" + id +"\" onclick=\"mudaJogo('"
+        + callback + "')\" type=\"button\"></button></div>"
+}
 
-	for(var i = 1; i< NumJogos; i++){ 
-		botao = document.getElementById("botao" + i);
-		if(getQueryVariable("campoEscondidoJogo"+i) == 1 && botao != null){
-			botao.style.visibility = "visible";
+function configButton() {
+	var btJogos = [
+        'Jogo_1-Formas Sombras', 'Jogo_2-Formas Coloridas', 'Jogo_3-Formas Azuis', 'Jogo_4-Turma da Monica2',
+        'Jogo_5-Bolas', 'Jogo_6-Sorvete', 'Jogo_7-Pirulito', 'Jogo_8-Carrinho',
+        'Jogo_9-Caminhaozinho', 'Jogo_10-Castelinho', 'Jogo_11-Sombras Abstratas', 'Jogo_12-Arvores',
+        'Jogo_13-Caixas', 'Jogo_14-Frutas 1', 'Jogo_15-Frutas 2', 'Jogo_16-Flor',
+        'Jogo_17-Turma da Monica', 'Jogo_18-ABC', 'Jogo_19-EFG', 'Jogo_20-123',
+        'Jogo_21-456', 'Jogo_22-789', 'Jogo_23-Peixinho', 'Jogo_24-Bob Esponja',
+        'Jogo_25-Labirinto', 'Jogo_26-Rosto', 'Jogo_27-NumeroBolas', 'Jogo_28-BolaColorida',
+        'Jogo_29-MateriaisEscolares', 'Jogo_30-Utensilios', 'Jogo_31-Insetos', 'Jogo_32-Passaro',
+        'Jogo_33-Passarinhos', 'Jogo_34-Trenzinho', 'Jogo_35-BotoesColoridos', 'Jogo_36-FormasSombras2',
+        'Jogo_37-SorveteSequencial', 'Jogo_38-BotoesNumeros', 'Jogo_39-BotoesLetras', 'JogoBonus-Lince',
+        'Jogo_41-VariasFormas', 'Jogo_42-VariasFrutas', 'Jogo_43-SorteveSemNumero',/*Jogo_44-NumerosParesEImpares(incompleto)*/,
+        'Jogo_45-PeixinhoSequencial', 'Jogo_46-VariasFormasSombras', 'Jogo_47-IdentificarODiferente-Bola', /*Jogo_48-BolaColorida-Lvl2(incompleto)*/,
+        /*Jogo_49-InsetosLvl2(incompleto)*/,'Jogo_50-IdentificarODiferente-Arvore', 'Jogo_51-TV', 'Jogo_52-Colher',
+        'Jogo_53-PassaroAzul', 'Jogo_54-IdentificarODiferente-Passaro', 'Jogo_55-IdentificarODiferente-TdaMonica',
+        'Jogo_56-Trenzinho%202', 'Jogo_57-Polvo', 'Jogo_58-AEIOU', 'Jogo_59-Porco',
+        'Jogo_60-Alfabetizacao1', 'Jogo_61-Alfabetizacao2', 'Jogo_62-Alfabetizacao3', 'Jogo_63-Alfabetizacao4',
+        'Jogo_64-Alfabetizacao5'
+	];
+
+	for(var i = 1; i<= btJogos.length; i++){
+		if(getQueryVariable("campoEscondidoJogo" + i) == 1){
+			$(criarBotao(btJogos[i - 1],i)).appendTo('#rowButtons');
 		}
 	}
 }
